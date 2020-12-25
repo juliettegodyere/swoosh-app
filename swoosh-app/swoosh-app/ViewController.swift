5e83787b99157f681b0dae31ff5206f2698809a4
Nkwor Juliet <nkworjuliet@omotayos-MacBook-Pro.local>
2020-12-26 00:23:20 +0100

    commit for lecture 5

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -21,7 +21,11 @@ class ViewController: UIViewController {
 //        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
 //
 //        bgImg.frame = view.frame
-    }
+        
+        }
+        @IBAction func  unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+
+        }
 
 
 }
